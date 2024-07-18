dc529d5389d9252d53b77e2b59dc9babf44af287
ozguevara@onyxerp.com <ozguevara@onyxerp.com>
2024-07-18 15:50:19 -0500

    Modified 2 files

--- .spectral.mjs
+++ .spectral.mjs
@@ -1,2 +1,2 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE0OTc0MA/spectral.js?branch=master&token=b358c2a8-922f-441f-95bb-0fae334021f5";
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE0OTc0MA/spectral.js?branch_slug=master&token=b358c2a8-922f-441f-95bb-0fae334021f5";
 export default { extends: ruleset };
